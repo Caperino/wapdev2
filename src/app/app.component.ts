24923a00fe9163f6dab7a91d85471689e4bd27c4
Timo Kappel <timo.kappel@gmail.com>
2023-12-27 15:41:03 +0100

    post-lecture saving

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+import {UserService} from "./user.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatMenuModule, RouterLink, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Exercise3';
+
+  constructor(private router:Router, public userService:UserService) {
+  }
+
+  executeLogout(): void {
+    localStorage.removeItem('access_token');
+    this.router.navigate(['login']);
+  }
 }
